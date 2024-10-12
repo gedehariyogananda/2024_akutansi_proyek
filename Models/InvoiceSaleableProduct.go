a275741f8719a2ea6ed46e8d446cf8bcc18a2215
gedehariyogananda <gedehariyoga@gmail.com>
2024-10-12 11:27:09 +0700

    fix: model setup invoice topings

--- Models/InvoiceSaleableProduct.go
+++ Models/InvoiceSaleableProduct.go
@@ -1,13 +1,14 @@
 package Models
 
 type InvoiceSaleableProduct struct {
-	InvoiceID         string          `json:"invoice_id"`
-	SaleableProductID string          `json:"saleable_product_id"`
-	QuantitySold      int             `json:"quantity_sold"`
-	CompanyID         string          `json:"-"`
-	Notes             string          `json:"notes"`
-	Invoice           Invoice         `gorm:"foreignKey:InvoiceID" json:"-"`
-	SaleableProduct   SaleableProduct `gorm:"foreignKey:SaleableProductID" json:"saleable_product"`
-	Company           Company         `gorm:"foreignKey:CompanyID" json:"-"`
+	InvoiceID               string                `json:"invoice_id"`
+	SaleableProductID       string                `json:"saleable_product_id"`
+	QuantitySold            int                   `json:"quantity_sold"`
+	CompanyID               string                `json:"-"`
+	SaleableProductTopingID string                `json:"saleable_product_toping_id"`
+	Notes                   string                `json:"notes"`
+	Invoice                 Invoice               `gorm:"foreignKey:InvoiceID" json:"-"`
+	SaleableProductToping   SaleableProductToping `gorm:"foreignKey:SaleableProductTopingID" json:"saleable_product_toping"`
+	SaleableProduct         SaleableProduct       `gorm:"foreignKey:SaleableProductID" json:"saleable_product"`
+	Company                 Company               `gorm:"foreignKey:CompanyID" json:"-"`
 }
-
--- Models/InvoiceSaleableToping.go
+++
@@ -1,8 +0,0 @@
-package Models
-
-type InvoiceSaleableToping struct {
-	InvoiceSaleableProductID string                 `json:"invoice_saleable_product_id"`
-	SaleableProductTopingID  string                 `json:"saleable_product_toping_id"`
-	InvoiceSaleableProduct   InvoiceSaleableProduct `gorm:"foreignKey:InvoiceSaleableProductID" json:"invoice_saleable_product"`
-	SaleableProductToping    SaleableProductToping  `gorm:"foreignKey:SaleableProductTopingID" json:"saleable_product_toping"`
-}
