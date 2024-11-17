c55f5bda8ed3d8503062e9b61024e75ad6d13d7e
riyan athariq <riyan.athariq@privyid.tech>
2024-11-17 21:36:06 +0700

    feat : init mongodb and webhook product

---
+++ Dependencies/Options.go
@@ -0,0 +1,22 @@
+package Dependencies
+
+import "2024_akutansi_project/Config"
+
+type Option func(provider *Dependency)
+
+func WithDB() Option {
+	return func(deps *Dependency) {
+		Config.Connect()
+		db := Config.DB
+		if db == nil {
+			panic("Failed to connect to database!")
+		}
+		deps.DB = db
+	}
+}
+
+func WithMongo() Option {
+	return func(deps *Dependency) {
+		deps.Mongo = Config.InitMongoDB()
+	}
+}
