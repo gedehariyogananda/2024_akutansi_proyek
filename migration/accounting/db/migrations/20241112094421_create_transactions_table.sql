538df7babc015121ba2c1fc1ad64a5aea2d47adc
gedehariyogananda <gedehariyoga@gmail.com>
2024-12-12 19:58:43 +0700

    fix: transaction fields

--- migration/accounting/db/migrations/20241112094421_create_transactions_table.sql
+++ migration/accounting/db/migrations/20241112094421_create_transactions_table.sql
@@ -7,13 +7,12 @@ CREATE TABLE transactions
     title                   VARCHAR(50)              NOT NULL,
     name                    VARCHAR(255)             NOT NULL,
     additional_data         JSONB,
-    type                    VARCHAR(255)             NOT NULL,
     date                    TIMESTAMP WITH TIME ZONE NOT NULL,
     due_date                TIMESTAMP WITH TIME ZONE,
     note                    VARCHAR(255),
-    transaction_record_code VARCHAR(255),
+    transaction_record_code VARCHAR(255), -- TGL_BULAN_TAHUN-(ABC)-3NAMINGCOMPANY
     transaction_id          VARCHAR(100),
-    activity_type           VARCHAR(255)             NOT NULL,
+    payment_method           VARCHAR(255)             NOT NULL,
     payment_type            VARCHAR(255)             NOT NULL,
     amount                 NUMERIC(20, 2)           NOT NULL
 );
