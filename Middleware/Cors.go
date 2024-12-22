0e58c53489a95b72c236ff6d1b8f28d7813b1339
gedehariyogananda <gedehariyoga@gmail.com>
2024-12-22 10:01:47 +0700

    fix: cors handle

--- Middleware/Cors.go
+++ Middleware/Cors.go
@@ -7,8 +7,10 @@ import (
 
 func SetupCORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "User-Agent", "Content-Length", "Authorization"},
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "User-Agent", "Content-Length", "Authorization"},
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowCredentials: true,
+		// AllowAllOrigins:  true,
 	})
 }
