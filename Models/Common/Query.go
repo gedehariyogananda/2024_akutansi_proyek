5b89f473e0d03f1ad683b2c3f0408cfa7d9c74f1
handarudwiki <handarudwiki04@gmail.com>
2025-01-12 10:01:11 +0700

    fix filter unit

--- Models/Common/Query.go
+++ Models/Common/Query.go
@@ -2,9 +2,10 @@ package Common
 
 type Query struct {
 	Search         *string `json:"search"`
-	IsLocked       bool    `json:"is_locked"`
-	Status         bool    `json:"status"`
+	IsLocked       *bool   `json:"is_locked"`
+	Status         *bool   `json:"status"`
 	Limit          int     `json:"limit"`
+	TypeAccount    *string `json:"type_account"`
 	Page           int     `json:"page"`
 	CompanyID      *string `json:"company_id"`
 	SmallestUnitID string  `json:"smallest_unit_id"`
