1ae93bd1ea089d3d169f19322e1b8e0aca16cd97
handarudwiki <handarudwiki04@gmail.com>
2025-01-19 13:39:20 +0700

    feat: laba rugi

--- Routes/JournalEntryRoute.go
+++ Routes/JournalEntryRoute.go
@@ -2,6 +2,7 @@ package Routes
 
 import (
 	"2024_akutansi_project/Routes/Di"
+
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
@@ -19,4 +20,5 @@ func JournalEntriesRoutes(c *gin.RouterGroup, db *gorm.DB, redis *redis.Client) 
 	route.GET("/", controller.FindAll)
 	route.GET("/trial-balance", controller.TrialBalanceReport)
 	route.GET("/financial-balance", controller.FinancialBalanceReport)
+	route.GET("/profit-or-loss", controller.ProfitOrLossReport)
 }
